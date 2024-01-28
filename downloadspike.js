3282fb760ac9c000a5d26042d2e240ee5b355337
Tsuyoshi Ushio <ushio@simplearchitect.com>
2024-01-28 11:42:47 -0800

    Update to the fetch

--- downloadspike.js
+++ downloadspike.js
@@ -1,46 +1,25 @@
-const https = require('https');
 const fs = require('fs');
 const AdmZip = require('adm-zip');
+const { pipeline } = require('stream');
+const { promisify } = require('util');
 
-const url = 'https://github.com/TsuyoshiUshio/BlogBabel/releases/download/v1.0.0-preview.5/BlogBabel-win-x64.zip'; // 'https://github.com/TsuyoshiUshio/BlogBabel/releases/download/v1.0.0-preview.5/BlogBabel-win-x64.zip'; // Replace with your ZIP file's URL
-const outputPath = 'downloaded.zip'; // Temporary output path for the downloaded ZIP file
-const extractPath = 'extracted/'; // Directory where the contents will be extracted
+const url = 'https://github.com/TsuyoshiUshio/BlogBabel/releases/download/v1.0.0-preview.5/BlogBabel-win-x64.zip';
+const outputPath = 'downloaded.zip';
+const extractPath = 'extracted/';
 
-https.get(url, (response) => {
-  if (response.statusCode !== 200 && response.statusCode !== 302) {
-    console.error(`Failed to download file: ${response.statusCode}`);
-    response.resume();
-    return;
-  }
+const download = async (url, outputPath) => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+  await promisify(pipeline)(response.body, fs.createWriteStream(outputPath));
+  console.log('Download completed, extracting...');
+};
 
-  if (response.statusCode == 302) {
-    const newUrl = response.headers.location;
-    https.get(newUrl, (response) => {
-      if (response.statusCode !== 200 && response.statusCode !== 302) {
-        console.error(`Failed to download file: ${response.statusCode}`);
-        response.resume();
-        return;
-      }
-    
-      const fileStream = fs.createWriteStream(outputPath);
-      response.pipe(fileStream);
-    
-      fileStream.on('finish', () => {
-        fileStream.close();
-        console.log('Download completed, extracting...');
-    
-        // Unzip the file
-        const zip = new AdmZip(outputPath);
-        zip.extractAllTo(extractPath, true);
-        console.log(`Extraction completed to "${extractPath}"`);
-    
-        // Optionally, delete the downloaded ZIP file after extraction
-        fs.unlinkSync(outputPath);
-        console.log('Downloaded ZIP file removed.');
-      });
-    })
-  };
-
-}).on('error', (err) => {
-  console.error(`Error: ${err.message}`);
-});
+download(url, outputPath)
+  .then(() => {
+    const zip = new AdmZip(outputPath);
+    zip.extractAllTo(extractPath, true);
+    console.log(`Extraction completed to "${extractPath}"`);
+    fs.unlinkSync(outputPath);
+    console.log('Downloaded ZIP file removed.');
+  })
+  .catch((err) => console.error(`Error: ${err.message}`));
